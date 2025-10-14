0cb63563f04b8895ac61eeb1e7a236def6387cd6
Joe Finney <josephfinney@LIVE.COM>
2025-10-13 20:11:54 -0500

    New TrayIcon API and net9

--- Tradio/App.xaml.cs
+++ Tradio/App.xaml.cs
@@ -6,101 +6,90 @@ using Tradio.Controls;
 using Tradio.ViewModels;
 using WinUIEx;
 
-namespace Tradio
+namespace Tradio;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    public partial class App : Application
+    private TrayIcon? _trayIcon;
+    private readonly PlayerViewModel _playerVm = new();
+
+    public App()
     {
-        private Window? _window;
-        private readonly PlayerViewModel _playerVm = new();
+        InitializeComponent();
+        _playerVm.PropertyChanged += PlayerVmOnPropertyChanged;
+    }
 
-        public App()
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        InitializeTrayIcon();
+        await UpdateTrayIconAsync();
+    }
+
+    private void PlayerVmOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(PlayerViewModel.IsPlaying))
         {
-            InitializeComponent();
-            _playerVm.PropertyChanged += PlayerVmOnPropertyChanged;
+            UpdatePlayPauseCommandText();
+            // Update tray icon to reflect play/pause state
+            _ = UpdateTrayIconAsync();
         }
+    }
 
-        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    private void InitializeTrayIcon()
+    {
+        _trayIcon = new(0, "Assets/Radio.ico", "Tradio");
+        _trayIcon.Selected += TrayIcon_Selected;
+        _trayIcon.ContextMenu += TrayIcon_ContextMenu;
+        _trayIcon.IsVisible = true;
+    }
+
+    private void TrayIcon_ContextMenu(TrayIcon sender, TrayIconEventArgs args)
+    {
+        Flyout flyout = new()
         {
-            InitializeTrayIcon();
-            await UpdateTrayIconAsync();
+            Content = new OptionsControl()
+        };
+
+        args.Flyout = flyout;
+    }
+
+    private void TrayIcon_Selected(TrayIcon sender, TrayIconEventArgs args)
+    {
+        _playerVm.Toggle();
+        _ = UpdateTrayIconAsync();
+    }
+
+    private async Task UpdateTrayIconAsync()
+    {
+        if (_trayIcon is null)
+            return;
+
+        // Choose icon based on play state. When not playing, use Radio-Off.png
+        // Fallback to Radio.ico when playing or if the PNG isn't present.
+        string iconUri = _playerVm.IsPlaying
+            ? "Assets/Radio.ico"
+            : "Assets/Radio-Off.ico";
+
+        // TODO: maybe make this a little more robust witha try/catch
+        // _window.SetTaskBarIcon(Icon.FromFile(iconUri));
+        _trayIcon.SetIcon(iconUri);
+    }
+
+    private void UpdatePlayPauseCommandText()
+    {
+        if (_trayIcon is null)
+            return;
+
+        if (_playerVm.IsPlaying)
+        {
+            _trayIcon.Tooltip = "Tradio (Playing) - Click to Pause";
         }
-
-        private void PlayerVmOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        else
         {
-            if (e.PropertyName == nameof(PlayerViewModel.IsPlaying))
-            {
-                UpdatePlayPauseCommandText();
-                // Update tray icon to reflect play/pause state
-                _ = UpdateTrayIconAsync();
-            }
-        }
-
-        private void InitializeTrayIcon()
-        {
-            _window = new Window
-            {
-                Title = "Tradio"
-            };
-            WindowManager wm = WindowManager.Get(_window);
-            wm.TrayIconInvoked += Wm_TrayIconInvoked;
-            _window.AppWindow.SetTaskbarIcon("Assets/Radio.ico");
-            wm.IsVisibleInTray = true; // Show app in tray
-                                       // Minimize to tray:
-            wm.WindowStateChanged += (s, state) =>
-                wm.AppWindow.IsShownInSwitchers = state != WindowState.Minimized;
-
-            wm.WindowState = WindowState.Minimized; // Delay activating the window by starting minimized
-        }
-
-        private void Wm_TrayIconInvoked(object? sender, TrayIconInvokedEventArgs e)
-        {
-            if (e.Type == TrayIconInvokeType.RightMouseUp)
-            {
-                Flyout flyout = new()
-                {
-                    Content = new OptionsControl()
-                };
-                e.Flyout = flyout;
-            }
-            else if (e.Type == TrayIconInvokeType.LeftMouseUp)
-            {
-                _playerVm.Toggle();
-                _ = UpdateTrayIconAsync();
-            }
-        }
-
-        private async Task UpdateTrayIconAsync()
-        {
-            if (_window is null)
-                return;
-
-            // Choose icon based on play state. When not playing, use Radio-Off.png
-            // Fallback to Radio.ico when playing or if the PNG isn't present.
-            string iconUri = _playerVm.IsPlaying
-                ? "Assets/Radio.ico"
-                : "Assets/Radio-Off.ico";
-
-            // TODO: maybe make this a little more robust witha try/catch
-            // _window.SetTaskBarIcon(Icon.FromFile(iconUri));
-            _window.AppWindow.SetTaskbarIcon(iconUri);
-        }
-
-        private void UpdatePlayPauseCommandText()
-        {
-            if (this._window is null)
-                return;
-
-            if (_playerVm.IsPlaying)
-            {
-                _window.Title = "Tradio - Pause";
-            }
-            else
-            {
-                _window.Title = "Tradio - Play";
-            }
+            _trayIcon.Tooltip = "Tradio - Play";
         }
     }
 }
